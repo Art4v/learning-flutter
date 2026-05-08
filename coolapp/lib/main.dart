084f384e3f8cce4fe8a22cbc7ca6ae133a3ef6ab
Art4v <aarav_20@outlook.com>
2026-05-08 12:27:45 +1000

    created first container

--- coolapp/lib/main.dart
+++ coolapp/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart'; // gives access to prebuilt widgets and material design components
 
 void main() {
-  runApp(MyApp()); // entry point of the app, runs the MyApp widget which inflates to the size of whichever screen we are running on
-
+  runApp(
+    MyApp(),
+  ); // entry point of the app, runs the MyApp widget which inflates to the size of whichever screen we are running on
 }
 
 // stateless widget = no dynamic data
@@ -11,16 +12,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     // build method = describes the UI
-    return MaterialApp( // used as the root of the application, allowing us to configure themes and routes
+    return MaterialApp(
+      // used as the root of the application, allowing us to configure themes and routes
       // scaffold = visual layout structure
       home: Scaffold(
+        // appbar widget
         appBar: AppBar(
           backgroundColor: Colors.green,
           title: const Text('Flutter is Fun!'),
         ),
-      )
+
+        // center widget = called by right-clicking and selecting 'refactor'
+        body: Center(
+          // container widget = similar to divs in html
+          child: Container(
+            child: const Text('Hello, World.'),
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+          ),
+        ),
+      ),
     );
   }
 }
